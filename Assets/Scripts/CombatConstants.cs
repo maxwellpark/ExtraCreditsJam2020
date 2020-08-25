5f5de41bf57b70b0ab5001b2c4d9d66bb673a395
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2020-08-25 16:55:48 +0100

    gitattributes updated

--- Assets/Scripts/CombatConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CombatConstants : MonoBehaviour
-{
-    public static int basicProjectileDamage = 1; 
-}
